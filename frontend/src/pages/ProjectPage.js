d1815ddd67dbb7f9fc9ba15dcaba64d360ede327
Gaurav Singh <111694602+grv-sngh@users.noreply.github.com>
2024-05-29 11:07:06 +0000

    Base App Completed

--- frontend/src/pages/ProjectPage.js
+++ frontend/src/pages/ProjectPage.js
@@ -1,12 +1,37 @@
 // frontend/src/pages/ProjectPage.js
-import React from 'react';
+
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const ProjectPage = () => {
-  return (
-    <div>
-      <h2>Project</h2>
-    </div>
-  );
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        const fetchProjects = async () => {
+            try {
+                const response = await axios.get('https://silver-happiness-jv9x5959v77cvqg-5000.app.github.dev/projects');
+                setProjects(response.data);
+            } catch (error) {
+                console.error('Error fetching projects:', error);
+            }
+        };
+
+        fetchProjects();
+    }, []);
+
+    return (
+        <div>
+            <h2>Projects</h2>
+            <ul>
+                {projects.map(project => (
+                    <li key={project._id}>
+                        <h2>{project.title}</h2>
+                        <p>{project.description}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 };
 
 export default ProjectPage;
